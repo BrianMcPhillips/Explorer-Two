970cd9b206787306073aed6bc2d6d6c9ea2a4d27
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-02 19:16:00 -0700

    Endpoints set up and returning location on front end

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/location', (req, res) => {
+  const userInput = req.query.search;
+  console.log(userInput);
+  res.json({
+    formatted_query: 'Seattle, WA, USA',
+    latitude: '47.606210',
+    longitude: '-122.332071'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+})
